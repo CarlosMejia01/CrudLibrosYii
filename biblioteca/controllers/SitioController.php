57c530046aa95ae4c589f483c9e6449c015a4234
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-21 13:47:39 -0500

    Creación de formulario y validacion de campos

--- biblioteca/controllers/SitioController.php
+++ biblioteca/controllers/SitioController.php
@@ -7,14 +7,24 @@ use Yii;
 use yii\filters\AccessController;
 use yii\web\Controller;
 
+use app\models\FormularioForm;
+
 class SitioController extends Controller
 {
-    
-    public function actionInicio(){
-        return $this->render('inicio');
+
+    public function actionInicio()
+    {
+
+        $model = new FormularioForm;
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+
+            $suma = $model->valora+$model->valorb;
+            $valorRespuesta = ("El resultado es:".$suma);
+
+            return $this->render('inicio', ['mensaje' => $valorRespuesta, 'model' => $model]);
+        }
+
+        return $this->render('inicio', ['mensaje' => "", 'model' => $model]);
     }
-
 }
-
-
-?>
--- biblioteca/views/sitio/inicio.php
+++ biblioteca/views/sitio/inicio.php
@@ -2,8 +2,30 @@
 
 
 use yii\helpers\Html;
-
+use yii\widgets\ActiveForm;
 
 ?>
 
-Hola Usuario
+
+<?php
+
+if ($mensaje) {
+    echo Html::tag('div', Html::encode($mensaje), ['class' => 'alert alert-danger']);
+}
+
+?>
+
+<?php $formulario = ActiveForm::begin(); ?>
+
+<?= $formulario->field($model, 'valora') ?>
+<?= $formulario->field($model, 'valorb') ?>
+
+
+<div class="form-group">
+
+    <?= Html::submitButton('Enviar', ['class' => 'btn btn-primary']) ?>
+
+
+</div>
+
+<?php ActiveForm::end(); ?>
