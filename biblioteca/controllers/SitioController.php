f58b1b902ba228683e29b02e05d1b51a8cd9ff5f
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-21 13:03:30 -0500

    Creación controller y vista sitio

---
+++ biblioteca/controllers/SitioController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+
+use Yii;
+use yii\filters\AccessController;
+use yii\web\Controller;
+
+class SitioController extends Controller
+{
+    
+    public function actionInicio(){
+        return $this->render('inicio');
+    }
+
+}
+
+
+?>
